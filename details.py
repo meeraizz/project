e9cb40b1d24abe5df88e3bdb22f6be31481aef0f
Ameera Izzati <ameeraizzaty05@gmail.com>
2024-05-09 19:06:01 +0800

    change line 15 to 66

--- details.py
+++ details.py
@@ -12,30 +12,58 @@ class DetailsClass:
         self.root.focus_force()
 
         #=====title======
-        title=Label(self.root,text="Manage Student Details", font=("times new roman",20,"bold"),bg="#033054",fg="white").place(x=0,y=0,relwidth=1,height=50)
+        title=Label(self.root,text="Manage Student Details", font=("times new roman",20,"bold"),bg="#033054",fg="white").place(x=0,y=0,relwidth=1,height=45)
 
         #=====variables====
         self.var_roll=StringVar()
-        self.var_duration=StringVar()
-        self.var_charges=StringVar()
+        self.var_name=StringVar()
+        self.var_email=StringVar()
+        self.var_gender=StringVar()
+        self.var_dob=StringVar()
+        self.var_contact=StringVar()
+        self.var_course=StringVar()
+        self.var_a_date=StringVar()
+        self.var_state=StringVar()
+        self.var_city=StringVar()
+        self.var_pin=StringVar()
 
         #=====widgets======
+        #=======column 1=========
         lbl_roll=Label(self.root,text="Roll No.", font=("goudy old style",15,"bold"),bg="white").place(x=10,y=60)
         lbl_name=Label(self.root,text="Name",font=("goudy old style",15,"bold"),bg="white").place(x=10,y=100)                     
         lbl_email=Label(self.root,text="Email",font=("goudy old style",15,"bold"),bg="white").place(x=10,y=140)
         lbl_gender=Label(self.root,text="Gender",font=("goudy old style",15,"bold"),bg="white").place(x=10,y=180)
+        lbl_state=Label(self.root,text="State",font=("goudy old style",15,"bold"),bg="white").place(x=10,y=220)
+        lbl_address=Label(self.root,text="Address",font=("goudy old style",15,"bold"),bg="white").place(x=10,y=260)
 
         #====entry fields====
         self.txt_roll=Entry(self.root,textvariable=self.var_roll,font=("goudy old style",15,"bold"),bg="lightyellow")
         self.txt_roll.place(x=150,y=60,width=200)
-        txt_duration=Entry(self.root,textvariable=self.var_duration,font=("goudy old style",15,"bold"),bg="lightyellow").place(x=150,y=100,width=200)
-        txt_charges=Entry(self.root,textvariable=self.var_charges,font=("goudy old style",15,"bold"),bg="lightyellow").place(x=10,y=140,width=200)
-        self.txt_description=Text(self.root,font=("goudy old style",15,"bold"),bg="lightyellow")
-        self.txt_description.place(x=150,y=180,width=500,height=130)
+        txt_name=Entry(self.root,textvariable=self.var_name,font=("goudy old style",15,"bold"),bg="lightyellow").place(x=150,y=100,width=200)
+        txt_email=Entry(self.root,textvariable=self.var_email,font=("goudy old style",15,"bold"),bg="lightyellow").place(x=150,y=140,width=200)
+        self.txt_gender=ttk.Combobox(self.root,textvariable=self.var_gender,values=("Select","Male","Female"),font=("goudy old style",15,"bold"),state='readonly',justify=CENTER)
+        self.txt_gender.place(x=150,y=180,width=200,)
+        self.txt_gender.current(0)
 
+       #=======column 2=========
+        lbl_dob=Label(self.root,text="D.O.B", font=("goudy old style",15,"bold"),bg="white").place(x=360,y=60)
+        lbl_contact=Label(self.root,text="Contact",font=("goudy old style",15,"bold"),bg="white").place(x=360,y=100)                     
+        lbl_admission=Label(self.root,text="Admission",font=("goudy old style",15,"bold"),bg="white").place(x=360,y=140)
+        lbl_course=Label(self.root,text="Course",font=("goudy old style",15,"bold"),bg="white").place(x=360,y=180)
 
-
-
+        #====entry fields 2====
+        self.course_list=[]
+        #function_call to update the list
+        txt_dob=Entry(self.root,textvariable=self.var_dob,font=("goudy old style",15,"bold"),bg="lightyellow").place(x=480,y=60,width=200)
+        txt_contact=Entry(self.root,textvariable=self.var_contact,font=("goudy old style",15,"bold"),bg="lightyellow").place(x=480,y=100,width=200)
+        txt_admission=Entry(self.root,textvariable=self.var_a_date,font=("goudy old style",15,"bold"),bg="lightyellow").place(x=480,y=140,width=200)
+        self.txt_course=ttk.Combobox(self.root,textvariable=self.var_course,values=(" "),font=("goudy old style",15,"bold"),state='readonly',justify=CENTER)
+        self.txt_course.place(x=480,y=180,width=200,)
+        self.txt_course.set("Empty")
+        
+        #====text address=====
+        self.txt_address=Text(self.root,font=("goudy old style",15,"bold"),bg="lightyellow")
+        self.txt_address.place(x=150,y=260,width=540,height=100)
 
 
 
---
+++ result.py
@@ -0,0 +1,20 @@
+from tkinter import *
+from PIL import Image, ImageTk    #pip install pillow
+from tkinter import ttk,messagebox
+import sqlite3
+
+class ResultClass:
+    def __init__(self,root):
+        self.root=root
+        self.root.title("Grade Master")
+        self.root.geometry("1200x480+80+170")
+        self.root.config(bg="white")
+        self.root.focus_force()
+
+        #=====title======
+        title=Label(self.root,text="Add Student Results", font=("times new roman",20,"bold"),bg="#033054",fg="white").place(x=0,y=0,relwidth=1,height=45)
+
+if __name__=="__main__":
+    root=Tk()
+    obj=ResultClass(root)
+    root.mainloop()    
