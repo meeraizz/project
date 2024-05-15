5e8f7fd914aa590892925e0f3459b462834063d1
adilahisham <nuradilahnorhisham@gmail.com>
2024-05-16 01:10:18 +0800

    Connect to database

--- student_grade.py
+++ student_grade.py
@@ -5,22 +5,22 @@ import sqlite3
 
 class resultClass:
     def __init__(self,root):
-        self.root=root
+        self.root = root
         self.root.title("Grade Master")
         self.root.geometry("1200x480+80+170")
         self.root.config(bg='white')
         self.root.focus_force()
 
         #===title====
-        title=Label(self.root,text="Add Student Result",font=("times new roman",20,"bold"),bg="pink",fg="#262626").place(x=10,y=15,width=1260,height=50)
+        title = Label(self.root,text="Add Student Result",font =("times new roman",20,"bold"),bg ="pink",fg ="#262626").place(x=10,y=15,width=1260,height=50)
 
         #====variables====
-        self.var_roll=StringVar
-        self.var_name=StringVar()
-        self.var_course=StringVar()
-        self.var_marks=StringVar()
-        self.var_full_marks=StringVar()
-        self.roll_list=[]
+        self.var_roll = StringVar
+        self.var_name = StringVar()
+        self.var_course = StringVar()
+        self.var_marks = StringVar()
+        self.var_full_marks = StringVar()
+        self.roll_list = []
 
         #===widgets===
         lbl_select = Label(self.root, text="Select Student", font=("times new roman", 20, "bold"), bg="white").place(x=50, y=100)
@@ -34,14 +34,15 @@ class resultClass:
         self.txt_student.set("Select")
         btn_search=Button(self.root,text='Search',font=("times new roman",15,"bold"),bg="#03a9f4",fg="white",cursor="hand2").place(x=500,y=100,width=100,height=28)
 
-        txt_name=Entry(self.root,textvariable=self.var_name,font=("times new roman",20,"bold"),bg="lightyellow",state='readonly').place(x=280,y=160,width=320)
-        txt_course=Entry(self.root,textvariable=self.var_course,font=("times new roman",20,"bold"),bg="lightyellow",state='readonly').place(x=280,y=220,width=320)
-        txt_marks=Entry(self.root,textvariable=self.var_marks,font=("times new roman",20,"bold"),bg="lightyellow").place(x=280,y=280,width=320)
-        txt_full_marks=Entry(self.root,textvariable=self.var_full_marks,font=("times new roman",20,"bold"),bg="lightyellow").place(x=280,y=340,width=320)
+        txt_name = Entry(self.root,textvariable=self.var_name,font=("times new roman",20,"bold"),bg="lightyellow",state='readonly').place(x=280,y=160,width=320)
+        txt_course = Entry(self.root,textvariable=self.var_course,font=("times new roman",20,"bold"),bg="lightyellow",state='readonly').place(x=280,y=220,width=320)
+        txt_marks = Entry(self.root,textvariable=self.var_marks,font=("times new roman",20,"bold"),bg="lightyellow").place(x=280,y=280,width=320)
+        txt_full_marks = Entry(self.root,textvariable=self.var_full_marks,font=("times new roman",20,"bold"),bg="lightyellow").place(x=280,y=340,width=320)
 
         #=====button======
-        btn_add=Button(self.root,text="Submit",font=("times new roman",15),bg="lightgreen",activebackground="lightgreen",cursor="hand2").place(x=300,y=420,width=120,height=35)
-        btn_add=Button(self.root,text="Clear",font=("times new roman",15),bg="lightgrey",activebackground="lightgrey",cursor="hand2").place(x=430,y=420,width=120,height=35)
+        btn_add = Button(self.root,text="Submit",font=("times new roman",15),bg="lightgreen",activebackground="lightgreen",cursor="hand2").place(x=300,y=420,width=120,height=35)
+        btn_add = Button(self.root,text="Clear",font=("times new roman",15),bg="lightgrey",activebackground="lightgrey",cursor="hand2").place(x=430,y=420,width=120,height=35)
+
 
 
 
@@ -57,6 +58,6 @@ class resultClass:
 
 
 if __name__=="__main__":
-    root=Tk()
-    obj=resultClass(root)
+    root = Tk()
+    obj = resultClass(root)
     root.mainloop()
