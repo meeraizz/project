19b859d13bbf1b64dfc2abf7c246af84b08aae2f
alyabatrisyiia <168154978+alyabatrisyiia@users.noreply.github.com>
2024-05-17 01:08:13 +0800

    Update login.py

--- login.py
+++ login.py
@@ -7,6 +7,8 @@ window.title("Login")
 window.geometry('340x440')
 # window.configure(bg='#333333')
 
+frame = tkinter.Frame(bg='#333333')
+
 login_label = tkinter.Label(window, text="Login", bg='#333333', fg="FFFFFF", font=("Arial", 16))
 username_label = tkinter.Label(window, text="Username", bg='#333333',fg="FFFFFF", font=("Arial", 16))
 username_entry = tkinter.Entry(window, font=("Arial", 16))
@@ -14,13 +16,13 @@ password_entry = tkinter.Entry(window, show="*", font=("Arial", 16))
 password_label = tkinter.Label(window, text="Password", bg='#333333', fg="FFFFFF", font=("Arial", 16))
 login_button = tkinter.Button(window, text="Login", bg="#FF3399", fg="#FFFFFF", font=("Arial", 16))
 
-login_label.grid(row=0, column=0, columnspan=2)
+login_label.grid(row=0, column=0, columnspan=2. sticky="news", pady=40)
 username_label.grid(row=1, column=0)
-username_entry.grid(row=1, column=1)
+username_entry.grid(row=1, column=1, pady=20)
 password_label.grid(row=2, column=0)
-password_entry.grid(row=2, column=1)
-login_button.grid(row=3, column=0, columnspan=2)
+password_entry.grid(row=2, column=1, pady=20)
+login_button.grid(row=3, column=0, columnspan=2, pady=30)
 
-
+frame.pack()
 
 window.mainloop()
