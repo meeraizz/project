dd888d6185c419bd8fc7deaf7f3b1c71d6dfdf6f
adilahisham <nuradilahnorhisham@gmail.com>
2024-05-24 09:55:52 +0800

    Widgets and Variables added into Teacher page

--- teacher's-profile.py
+++ teacher's-profile.py
@@ -12,10 +12,22 @@ class TeacherProfile:
         self.root.config(bg='white')
         self.root.focus_force()
 
+        # Variables
+        self.var_teacher_id = StringVar()
+        self.var_teacher_name = StringVar()
+        self.var_teacher_email = StringVar()
+        self.var_teacher_contact = StringVar()
+        self.course_list = []
 
         # Title
         title = Label(self.root, text="Teacher Profile", font=("times new roman", 20, "bold"), bg="pink", fg="#262626").place(x=10, y=15, width=780, height=50)
 
+        # Widgets
+        lbl_select = Label(self.root, text="Select Teacher", font=("times new roman", 20, "bold"), bg="white").place(x=50, y=100)
+        
+        self.txt_teacher = ttk.Combobox(self.root, textvariable=self.var_teacher_id, font=("times new roman", 15, "bold"), state='readonly', justify=CENTER)
+        self.txt_teacher.place(x=250, y=100, width=200)
+        self.txt_teacher.set("Select")
 
 
 if __name__ == "__main__":
