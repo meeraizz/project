4900f43a702370491f9da45fcd04cd7e57e378fb
adilahisham <nuradilahnorhisham@gmail.com>
2024-05-28 16:13:50 +0800

    exit

---
+++ dashboard.py
@@ -0,0 +1,4 @@
+from tkinter import*
+from PIL import Image,ImageTk
+from tkinter import ttk,messagebox
+import sqlite3
--- student_grade.py
+++ student_grade.py
@@ -12,7 +12,7 @@ class resultClass:
         self.root.focus_force()
 
         #===title====
-        title = Label(self.root,text="Add Student Result",font =("times new roman",20,"bold"),bg ="pink",fg ="#262626").place(x=10,y=15,width=1260,height=50)
+        title = Label(self.root,text="Add Student Result",font =("King",20,"bold"),bg ="#ff80b4",fg ="#262626").place(x=10,y=15,width=1260,height=50)
 
         #====variables====
         self.var_roll = StringVar()
@@ -33,7 +33,7 @@ class resultClass:
         self.txt_student = ttk.Combobox(self.root, textvariable=self.var_roll, values=self.roll_list, font=("times new roman", 15, "bold"), state='readonly', justify=CENTER)
         self.txt_student.place(x=280,y=100,width=200)
         self.txt_student.set("Select")
-        btn_search=Button(self.root,text='Search',font=("times new roman",15,"bold"),bg="#03a9f4",fg="white",cursor="hand2").place(x=500,y=100,width=100,height=28)
+        btn_search=Button(self.root,text='Search',font=("times new roman",15,"bold"),bg="#604abd",fg="white",cursor="hand2").place(x=500,y=100,width=100,height=28)
 
         txt_name = Entry(self.root,textvariable=self.var_name,font=("times new roman",20,"bold"),bg="lightyellow",state='readonly').place(x=280,y=160,width=320)
         txt_course = Entry(self.root,textvariable=self.var_course,font=("times new roman",20,"bold"),bg="lightyellow",state='readonly').place(x=280,y=220,width=320)
@@ -41,8 +41,8 @@ class resultClass:
         txt_full_marks = Entry(self.root,textvariable=self.var_full_marks,font=("times new roman",20,"bold"),bg="lightyellow").place(x=280,y=340,width=320)
 
         #=====button======
-        btn_add = Button(self.root,text="Submit",font=("times new roman",15),bg="lightgreen",activebackground="lightgreen",cursor="hand2").place(x=300,y=420,width=120,height=35)
-        btn_add = Button(self.root,text="Clear",font=("times new roman",15),bg="lightgrey",activebackground="lightgrey",cursor="hand2").place(x=430,y=420,width=120,height=35)
+        btn_add = Button(self.root,text="Submit",font=("King",15),bg="#e0d2ef",activebackground="lightgreen",cursor="hand2").place(x=300,y=420,width=120,height=35)
+        btn_add = Button(self.root,text="Clear",font=("King",15),bg="#ffb3d2",activebackground="lightgrey",cursor="hand2").place(x=430,y=420,width=120,height=35)
 
         #==========================================================
     def fetch_roll(self):
