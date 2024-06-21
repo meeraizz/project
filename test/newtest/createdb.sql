1d051da3ac480d59b2eb94c0a717411293f0cbbe
Ameera Izzati <ameeraizzaty05@gmail.com>
2024-06-21 23:20:03 +0800

    change calendar

--- test/newtest/createdb.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE emp(empid INTEGER NOT NULL PRIMARY KEY,empname TEXT NOT NULL,email NOT NULL);
-
-INSERT INTO emp(empid,empname,email)
-VALUES (1,"Sam","test@test.com");
-
-SELECT * FROM emp;
